bd9f4dc7af29ca1a82908e7c06082fb7913f63b7
woosweb <woosweb@gmail.com>
2022-11-02 10:06:21 +0900

    feat: apply swap

--- contracts.js
+++ contracts.js
@@ -1,4 +1,7 @@
 module.exports = {
   mainnet: {},
-  testnet: {},
+  testnet: {
+    routeswap:
+      'xpla1lnv94ddx6fmwancj47zkdpgquqfguh3havrdxgdmxf98jy0vxfdq0dgp0y',
+  },
 };
--- cw20/pairs.dex.js
+++ cw20/pairs.dex.js
@@ -1,4 +1,21 @@
 module.exports = {
   mainnet: {},
-  testnet: {},
+  testnet: {
+    xpla1ujxvgkf94dl4zjau0zmvp3uugg8efzq4qakz5q502etzw4q8f06s0nymuc: {
+      dex: 'dezswap',
+      type: 'xyk',
+      assets: [
+        'xpla14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s525s0h',
+        'axpla',
+      ],
+    },
+    xpla104yd6mjyfx90swm022h9t37g2c2ar2w7zkcee5qlkrfrdge0ka9qhesxhp: {
+      dex: 'dezswap',
+      type: 'xyk',
+      assets: [
+        'xpla14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s525s0h',
+        'xpla14vjqp6cq9mr90wzmqxsa5erx70h4xy5eep0jynjqmlstupngtghqmjya9p',
+      ],
+    },
+  },
 };
