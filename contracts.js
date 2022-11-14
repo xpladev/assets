3231f2577899b250dcbe7072ba33d01aa449fc47
woosweb <woosweb@gmail.com>
2022-11-14 09:44:55 +0900

    fix: apply routeswap

--- contracts.js
+++ contracts.js
@@ -1,5 +1,8 @@
 module.exports = {
-  mainnet: {},
+  mainnet: {
+    routeswap:
+      'xpla1uv4dz7ngaqwymvxggrjp3rnz3gs33szwjsnrxqg0ylkykqf8r7ns9s3cg4',
+  },
   testnet: {
     routeswap:
       'xpla1lnv94ddx6fmwancj47zkdpgquqfguh3havrdxgdmxf98jy0vxfdq0dgp0y',
