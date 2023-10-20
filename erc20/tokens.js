f1188256fb4927c78d3ceedf3d083da85857da2f
junhee0 <95892799+junhee0@users.noreply.github.com>
2023-10-20 06:48:38 +0000

    Update tokens.js
    add xNINKY

--- erc20/tokens.js
+++ erc20/tokens.js
@@ -9,5 +9,13 @@ module.exports = {
       icon: 'https://assets.xpla.io/icon/evm/ZAD.png',
       decimals: 6,
     },
+    '0xf04fa5A7e7883E48C5872D8121ABB94C63037Ea3': {
+      protocol: 'XPLA',
+      symbol: 'xNINKY',
+      name: 'xNINKY',
+      token: '0xf04fa5A7e7883E48C5872D8121ABB94C63037Ea3',
+      icon: 'https://assets.xpla.io/icon/evm/xNINKY.png',
+      decimals: 6,
+    },
   },
 };
