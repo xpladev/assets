516272fb704dff527e168e20550b83a1fc2d49e0
Felix-Baek <155944662+Felix-Baek@users.noreply.github.com>
2024-01-08 15:52:06 +0900

    Update cw721 contracts.js (testnet)
    Modify the contract address of BTB PADDLE

--- cw721/contracts.js
+++ cw721/contracts.js
@@ -92,7 +92,7 @@ module.exports = {
       homepage: 'https://evtlabs.io',
       marketplace: [],
     },
-    xpla1c94ss2232fm5v7cs36g5n56fd9vldflmg2l4g77qj3r86fdmqststr5eya: {
+    xpla160cwjchdcv5f8m0pevkur8jf2nd255necmk28vzf4uveztwfzqys6cyr4k: {
       contract:
         'xpla160cwjchdcv5f8m0pevkur8jf2nd255necmk28vzf4uveztwfzqys6cyr4k',
       name: 'BREAK THE BRICKS PADDLE',
