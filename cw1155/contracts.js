8de88902b5f01623951259c65625af7e3e21b449
Donghoon Kim <fly33499@gmail.com>
2025-01-07 11:31:20 +0900

    Update contracts.js
    MyB SBT contract updates

--- cw1155/contracts.js
+++ cw1155/contracts.js
@@ -1,5 +1,15 @@
 module.exports = {
-  mainnet: {},
+  mainnet: {
+    xpla18xsgaqx66wkljvnjcu57pfwq4dtjv4gay662j2pnhy46lvmzqycsxdtz54: {
+      contract:
+        'xpla18xsgaqx66wkljvnjcu57pfwq4dtjv4gay662j2pnhy46lvmzqycsxdtz54',
+      name: 'MyB Soulbound Tokens',
+      symbol: 'MyB SBT',
+      icon: 'https://resource.myb.im/myb_icon.png',
+      homepage: 'https://myb.im/',
+      marketplace: [],
+    },
+  },
   testnet: {
     xpla108pjpz7kf56646k2x3gr5qz544jphave7ftzyuww962u4cswwf8syeqegy: {
       contract:
