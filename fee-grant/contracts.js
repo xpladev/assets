e5f18770126b1860c73c2f4724a20aaae8ee4f4d
woosweb <woosweb@gmail.com>
2025-02-05 15:03:53 +0900

    feat: Added fee grant contract for XPLA GAMES

--- fee-grant/contracts.js
+++ fee-grant/contracts.js
@@ -42,5 +42,19 @@ module.exports = {
         send_amount: 100,
       },
     },
+    xpla1lkuwx0a4llqsjmwyhavdtr6t2smlgn5ng7pukszctvt8zfp0vwwqt3w6sg: {
+      contract:
+        'xpla1lkuwx0a4llqsjmwyhavdtr6t2smlgn5ng7pukszctvt8zfp0vwwqt3w6sg',
+      dapp_name: 'XPLA GAMES',
+      description:
+        'XPLA GAMES is a Playful Home Ground in Web3 Gaming Space. Blockchain gaming platform striving to create the ultimate Web3 gaming experience.',
+      organization: 'ZENAAD Inc.',
+      contact: 'contact@zenaad.com',
+      grant: {
+        total_amount: 2000000,
+        minimum_balance: 1000,
+        send_amount: 100,
+      },
+    },
   },
 };
